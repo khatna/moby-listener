3c6239d0d8a8c5831568ee4754060060f7af6974
Khatna Bold <khatna@github.com>
2023-02-17 00:55:36 +0800

    btc rpc working, get values of transactions

--- listener.go
+++ listener.go
@@ -1,22 +1,41 @@
 package main
 
 import (
-	"encoding/hex"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/pebbe/zmq4"
 )
 
 func main() {
+	fmt.Println(os.Getenv("BTC_RPC_HOST"))
+
+	// initialize rpc client
+	connCfg := &rpcclient.ConnConfig{
+		Host:         os.Getenv("BTC_RPC_HOST"),
+		User:         os.Getenv("BTC_RPC_USER"),
+		Pass:         os.Getenv("BTC_RPC_PASS"),
+		HTTPPostMode: true, // Bitcoin core only supports HTTP POST mode
+		DisableTLS:   true, // Bitcoin core does not provide TLS by default
+	}
+	client, err := rpcclient.New(connCfg, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Shutdown()
+
+	// connect to tcp sockets to listen to txos
 	zctx, _ := zmq4.NewContext()
 	sock, _ := zctx.NewSocket(zmq4.SUB)
-	defer sock.Close()
 	defer zctx.Term()
+	defer sock.Close()
 
-	err := sock.Connect("tcp://127.0.0.1:3600")
+	err = sock.Connect(os.Getenv("BTC_TCP_ENDPOINT"))
 
 	if err != nil {
-		fmt.Println("error while connecting to socket:", err)
+		log.Fatal(err)
 	}
 
 	sock.SetSubscribe("rawtx")
@@ -24,14 +43,21 @@ func main() {
 	// Receive messages
 	for {
 		msg, err := sock.RecvMessage(0)
-		rawtx, seq := msg[1], msg[2]
+
+		// the main encoded raw transaction is the second message
+		// in a multipart message:
+		// https://github.com/bitcoin/bitcoin/blob/master/doc/zmq.md
+		rawtx := msg[1]
 
 		if err != nil {
 			fmt.Println(err)
 			break
 		}
 
-		fmt.Println(hex.EncodeToString([]byte(rawtx)))
-		fmt.Println(hex.EncodeToString([]byte(seq)))
+		// Synchronously decode for now - no optimization necessary
+		// https://developer.bitcoin.org/reference/rpc/decoderawtransaction.html
+		rawtxResult, _ := client.DecodeRawTransaction([]byte(rawtx))
+
+		fmt.Printf("%v - %v BTC\n", rawtxResult.Hash, rawtxResult.Vout[0].Value)
 	}
 }
