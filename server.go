a447d0ded0d66097eccc535e38b84f119ae21f13
Khatanbuuvei Bold <khatna@github.com>
2023-03-01 23:59:11 +0800

    stream close conditions

--- server.go
+++ server.go
@@ -26,13 +26,27 @@ func (s *txHandlerService) GetTransactions(value *pb.Value, stream pb.TxHandler_
 	fmt.Println("New Connection")
 	conn := &connection{make(chan *pb.Tx), value.Value}
 	s.conns[conn] = struct{}{}
-	for tx := range conn.ch {
-		if err := stream.Send(tx); err != nil {
-			return err
+	defer delete(s.conns, conn)
+
+	// send messages over stream concurrently
+	errorCh := make(chan error)
+	go func() {
+		for tx := range conn.ch {
+			if err := stream.Send(tx); err != nil {
+				errorCh <- err
+			}
 		}
+	}()
+
+	// stop when the client is done, or there is an error while sending
+	select {
+	case err := <-errorCh:
+		fmt.Println("Error while sending transaction: ", err)
+		return err
+	case <-stream.Context().Done():
+		fmt.Println("Client done.")
+		return nil
 	}
-	delete(s.conns, conn)
-	return nil
 }
 
 // direct transactions to relevant connections
